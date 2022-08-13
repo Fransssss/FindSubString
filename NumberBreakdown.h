1536e1f21152c5706f02eedcfb387707659d34cb
Frans <97081479+Fransssss@users.noreply.github.com>
2022-08-13 16:45:44 -0700

    Update NumberBreakdown.h

--- NumberBreakdown.h
+++ NumberBreakdown.h
@@ -15,17 +15,17 @@ using std::endl;
 void breakdownNumber(const string& userNumber)
 {
     int oneDash = userNumber.find('-');                         // get first dash -
-    int twoDash = userNumber.find('-', oneDash + 1);        // find 2nd dash - from 1st dash -
+    int twoDash = userNumber.find('-', oneDash + 1);            // find 2nd dash - from 1st dash -
     cout << "\n== Phone Number Breakdown ==" << endl;
     // area code
     string codeArea = userNumber.substr(0, 3);
     // exchange code
-    string codeExch = userNumber.substr(oneDash + 1, 3);    // plus one means start from next digit
+    string codeExch = userNumber.substr(oneDash + 1, 3);       // plus one means start from next digit
     // line code
     string codeLine = userNumber.substr(twoDash+1, userNumber.size()-1);
 
     // put regions first then information
-    if(regionState(codeArea))                           // if only the number user input is really valid/exist
+    if(regionState(codeArea))                                  // if only the number user input is really valid/exist
     {
         cout << "The code area: ";
         cout << codeArea << endl;
